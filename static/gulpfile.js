3fa51ffc9a6a5e9eca09004373fc07ee41ea2b4a
Bulat Gabdrakhmanov <bulat.gab2000@gmail.com>
2020-07-18 21:19:50 +0300

    bulatgabdrakhmanov.github.io

---
+++ static/gulpfile.js
@@ -0,0 +1,51 @@
+const gulp          = require("gulp");
+const ts            = require("gulp-typescript");
+const sass          = require("gulp-sass");
+const gcmq          = require("gulp-group-css-media-queries");
+const csso          = require("gulp-csso");
+const rename        = require("gulp-rename");
+const autoprefixer  = require("gulp-autoprefixer");
+
+const inputFolder = "dev";
+const outputFolder = "prod";
+
+gulp.task("styles", function() {
+    return gulp.src(inputFolder + "/styles/main.scss")
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', '> 1%', 'ie >= 10', 'Firefox > 20'],
+            cascade: true,
+            grid: true
+        }))
+        .pipe(gcmq())
+        .pipe(csso())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(outputFolder + "/styles"));
+});
+
+gulp.task("scripts", function() {
+    return gulp.src(inputFolder + "/scripts/**/*.ts")
+        .pipe(ts())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(outputFolder + "/scripts"));
+});
+
+gulp.task("staticFiles", function(done) {
+    const fonts = gulp.src(inputFolder + "/fonts/**/*")
+        .pipe(gulp.dest(outputFolder + "/fonts"));
+
+    const img = gulp.src(inputFolder + "/img/**/*")
+        .pipe(gulp.dest(outputFolder + "/img"));
+
+    done();
+});
+
+gulp.task("build", gulp.parallel("styles", "scripts", "staticFiles"));
+
+gulp.task("watch", function(done) {
+    (gulp.series("build"))();
+    gulp.watch(inputFolder + "/styles/**/*.scss", gulp.parallel("styles"));
+    gulp.watch(inputFolder + "/scripts/**/*.ts", gulp.parallel("scripts"));
+    gulp.watch([inputFolder + "/img/**/*", inputFolder + "/fonts/**/*"], gulp.parallel("staticFiles"));
+    done();
+});
